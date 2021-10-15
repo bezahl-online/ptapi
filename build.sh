6c438408632a8fd1f21e444157c7e89a13c60910
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-10-15 18:31:37 +0200

    buildscript

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #/bin/bash
-(git pull && CGO_ENABLED=0 go build -o gm65server && echo "build successfull") || echo "build failed"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+(git pull && CGO_ENABLED=0 go build -o gm65server && echo -e "\n${GREEN}build successfull${NC}\n") || echo -e "\n${RED}build failed${NC}\n"
