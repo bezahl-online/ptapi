d5279a76097015352b2605b52bdf5c99d7416ac5
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-10-15 19:41:27 +0200

    build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-(git pull && CGO_ENABLED=0 go build -o gm65server && echo -e "\n${GREEN}build successfull${NC}\n") || echo -e "\n${RED}build failed${NC}\n"
+(git pull && CGO_ENABLED=0 go build -o ptapiserver && echo -e "\n${GREEN}build successfull${NC}\n") || echo -e "\n${RED}build failed${NC}\n"
