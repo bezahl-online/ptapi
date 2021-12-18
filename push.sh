e531a017722536eab4203798d43125b080e964df
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-12-18 07:41:28 +0100

    add docker-compose build

--- push.sh
+++ push.sh
@@ -5,4 +5,4 @@ NC='\033[0m' # No Color
 if [[ $(uname -m)!="armv7l" ]]; then echo -e "\n${RED}ERROR: build need to be done on 'ARMV7' plattform${NC}\n"; exit 1; fi
 version=$(cat version)
 echo "building version ${version}"
-(./build.sh && docker push www.greisslomat.at:5000/ptapi:${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
+(./build.sh && docker-compose build && docker push www.greisslomat.at:5000/ptapi:${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
