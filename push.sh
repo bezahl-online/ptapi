6801ab8e0d1f2cd5497792a6f8801cb8337611b0
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-04-02 16:47:18 +0200

    arch and version

--- push.sh
+++ push.sh
@@ -2,7 +2,9 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}ERROR: build need to be done on 'ARMV7' plattform${NC}\n"; exit 1; fi
+git pull
 version=$(cat version)
-echo "building version ${version}"
-(./build.sh && docker-compose build && docker push www.greisslomat.at:5000/ptapi:${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
+echo "building version $(arch)_${version}"
+# if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}ERROR: build need to be done on 'ARMV7' plattform${NC}\n"; exit 1; fi
+export PTAPI_VERSION=$(arch)_$(cat version)
+(./build.sh && docker-compose build && docker push www.greisslomat.at:5000/ptapi:$(arcg)_${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
