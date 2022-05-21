6a65d482322e618e4fe1cbc647baf8fc3d8990ee
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-05-21 22:46:01 +0200

    docker push

--- push.sh
+++ push.sh
@@ -2,9 +2,15 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-git pull
+git pull 
+REPRO="www.greisslomat.at:3307"
 version=$(cat version)
+PRG="ptapi"
 echo "building version $(arch)_${version}"
-# if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}ERROR: build need to be done on 'ARMV7' plattform${NC}\n"; exit 1; fi
-export PTAPI_VERSION=$(arch)_$(cat version)
-(./build.sh && docker-compose build && docker push www.greisslomat.at:5000/ptapi:$(arch)_${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
+export PTAPI_VERSION=$(arch)_$(cat version) # for docker-compose
+docker login https://www.greisslomat.at:3307 --username ralph --password natural-Kennwort
+(./build.sh && docker-compose build && docker push $REPRO/$PRG:$(arch)_${version} && echo -e "\n${GREEN}build successfull${NC}\n") || echo -e "\n${RED}build failed${NC}\n"
+if [ "${1}" == "latest" ]; then
+    docker tag $REPRO/$PRG:$(arch)_${version} $REPRO/$PRG:$(arch)_latest
+    docker push $REPRO/$PRG:x86_64_latest
+fi
